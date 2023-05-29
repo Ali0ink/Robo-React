005a3c4c39407b23e4ad4a23c38ee61230c82323
Ali0ink <alih033einy@gmail.com>
2023-05-29 13:33:14 +0330

    fixing some bugs

--- src/components/Scroll.jsx
+++ src/components/Scroll.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Scroll = (props) => {
   return (
-    <div style={{ overflowY: 'scroll', border: '2px solid blue', height: '500px' }}>
+    <div style={{ overflowY: 'scroll', border: '2px solid blue', height: '700px' }}>
         {props.children}
     </div>
   )
